28597f5e5b4922a74ccd5b45323431744ddb3c67
Marc Sladek <marc@sladek.me>
2021-06-11 18:13:01 +0200

    replace commons logger

--- src/main/java/com/celements/course/classcollections/migrator/AddDateValidationMigrator.java
+++ src/main/java/com/celements/course/classcollections/migrator/AddDateValidationMigrator.java
@@ -1,7 +1,7 @@
 package com.celements.course.classcollections.migrator;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.xwiki.component.annotation.Component;
 import org.xwiki.component.annotation.Requirement;
 import org.xwiki.model.reference.DocumentReference;
@@ -20,7 +20,7 @@ import com.xpn.xwiki.store.migration.XWikiDBVersion;
 @Component("AddDateValidationMigrator")
 public class AddDateValidationMigrator extends AbstractCelementsHibernateMigrator {
 
-  private static final Log LOGGER = LogFactory.getFactory().getInstance(
+  private static final Logger LOGGER = LoggerFactory.getLogger(
       AddDateValidationMigrator.class);
   
   @Requirement
